dfc09d9da687fa0cf91b80d6063b9d65f09f53f4
Jjgod Jiang <gzjjgod@gmail.com>
2012-01-26 23:24:00 +0100

    Basic working prototype

--- stream.js
+++ stream.js
@@ -1,30 +1,39 @@
-var n = 20, // number of layers
-    m = 200, // number of samples per layer
-    data0 = d3.layout.stack().offset("wiggle")(stream_layers(n, m)),
-    color = d3.interpolateRgb("#aad", "#556");
+d3.csv("commodity.csv", function(data) {
+    var n = 20, // number of layers
+        m = 200, // number of samples per layer
+        color = d3.interpolateRgb("#aad", "#556");
 
-var w = 960,
-    h = 500,
-    mx = m - 1,
-    my = d3.max(data0, function(d) {
-      return d3.max(d, function(d) {
-        return d.y0 + d.y;
-      });
+    var commodityTypes = [ "nickel", "lead", "iron", "gold", "copper", "zinc", "uranium", "tin", "silver" ];
+    var commodityData = commodityTypes.map(function(type) {
+        return data.map(function(row, i) {
+            return { x: i, y: +row[type] };
+        });
     });
+    var commodityPrices = d3.layout.stack().offset("silhouette")(commodityData);
 
-var area = d3.svg.area()
-    .x(function(d) { return d.x * w / mx; })
-    .y0(function(d) { return h - d.y0 * h / my; })
-    .y1(function(d) { return h - (d.y + d.y0) * h / my; });
+    var w = 1960,
+        h = 500,
+        mx = m - 1,
+        my = d3.max(commodityPrices, function(d) {
+          return d3.max(d, function(d) {
+            return d.y0 + d.y;
+          });
+        });
 
-var vis = d3.select("#chart")
-  .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    var area = d3.svg.area()
+        .x(function(d) { return d.x * w / mx; })
+        .y0(function(d) { return h - d.y0 * h / my; })
+        .y1(function(d) { return h - (d.y + d.y0) * h / my; });
 
-vis.selectAll("path")
-    .data(data0)
-  .enter().append("path")
-    .style("fill", function() { return color(Math.random()); })
-    .attr("d", area);
+    var vis = d3.select("#chart")
+      .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    vis.selectAll("path")
+        .data(commodityPrices)
+      .enter().append("path")
+        .style("fill", function() { return color(Math.random()); })
+        .attr("d", area);
+});
 
